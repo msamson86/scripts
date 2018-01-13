0108c4562c1905793b2cf2c11b9dd0791ce67a01
Michael Sass <mikesass@Michaels-MacBook-Pro-2.local>
2018-01-13 08:35:19 -0500

    iptables-counters.sh

---
+++ aux/iptables-counters.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#usage: iptables-counters.sh [target IP]
+
+#reset all counters and iptables rules
+iptables -Z && iptables -F
+#measure incoming traffic to [target ip]
+iptables -I INPUT 1 -s $1 -j ACCEPT
+# measure outgoing traffic to [target ip]
+iptables -I OUTPUT 1 -d $1 -j ACCEPT 
